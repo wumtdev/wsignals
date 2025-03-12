fece4b856059580154a748824b16489c08cb5f99
wumtdev <wumtdev@users.noreply.github.com>
2025-03-12 23:34:29 +0300

    improved async support

---
+++ example.py
@@ -0,0 +1,54 @@
+import asyncio
+from asyncio import CancelledError
+import time
+
+from wsignals import Signal
+
+
+DELAY = 3.0
+on_tick = Signal()
+stop = False
+
+def sync_timer():
+	while not stop:
+		print('sync_timer: sleeping')
+		time.sleep(DELAY)
+		print('sync_timer: calling')
+		on_tick.call()
+		print('sync_timer: called')
+
+async def timer():
+	while True:
+		print('timer: sleeping')
+		await asyncio.sleep(DELAY)
+		print('timer: calling')
+		on_tick.call()
+		print('timer: called')
+
+def sync_handler():
+	print('sync_handler: tick handled')
+
+async def async_handler():
+	print('async_handler: tick handled')
+
+async def main():
+	loop = asyncio.get_running_loop()
+	print('main: starting timer')
+	# asyncio.create_task(timer())
+	loop.run_in_executor(None, sync_timer)
+	print('main: timer started')
+
+	on_tick.connect(sync_handler)
+	on_tick.connect_async(async_handler)
+
+	try:
+		while True:
+			print('main: waiting tick')
+			await on_tick.next(future=loop.create_future())
+			print('main: tick handled')
+	except CancelledError:
+		global stop
+		stop = True
+
+if __name__ == '__main__':
+	asyncio.run(main())
--- wsignals/__init__.py
+++ wsignals/__init__.py
@@ -1,27 +1,62 @@
 import asyncio
-from typing import Callable, Coroutine, List
+from asyncio import AbstractEventLoop, Future
+from dataclasses import dataclass
+from typing import Callable, Coroutine, List, Optional
 
-type Listener = Callable[..., None | Coroutine[None, None, None]]
+
+type IListener = Callable[..., None | bool]
+type IAsyncCallback = Callable[..., Coroutine[None, None, None]]
+
+@dataclass
+class AsyncListener:
+	callback: IAsyncCallback
+	loop: AbstractEventLoop
+
+	def __call__(self, *args, **kwargs):
+		loop = self.loop
+		if loop.is_closed(): return True
+		loop.create_task(self.callback(*args, **kwargs))
+
+@dataclass
+class FutureListener:
+	future: Future
+
+	def __call__(self, *args, **kwargs):
+		future = self.future
+		loop = future.get_loop()
+		if future.done() or loop.is_closed(): return True
+		loop.call_soon_threadsafe(future.set_result, (args, kwargs))
+		return True
+
 class Signal:
-	listeners: List[Listener]
+	listeners: List[IListener]
 
 	def __init__(self):
 		self.listeners = list()
 	
-	def connect(self, listener: Listener):
+	def connect(self, listener: IListener):
 		self.listeners.append(listener)
 	
-	def __call__(self, listener: Listener) -> Listener:
+	def __call__(self, listener: IListener) -> IListener:
 		self.listeners.append(listener)
 		return listener
+	
+	def connect_async(self, callback: IAsyncCallback, loop: Optional[AbstractEventLoop] = None):
+		if not loop:
+			loop = asyncio.get_running_loop()
+		self.listeners.append(AsyncListener(callback=callback, loop=loop))
+	
+	def next(self, future: Optional[Future] = None, loop: Optional[AbstractEventLoop] = None) -> Future:
+		if not future:
+			if not loop: loop = asyncio.get_running_loop()
+			future = loop.create_future()
+		self.listeners.append(FutureListener(future=future))
+		return future
 
 	def call(self, *args, **kwargs):
-		try: loop = asyncio.get_running_loop()
-		except RuntimeError: loop = None
-		for listener in self.listeners:
-			coro = listener(*args, **kwargs)
-			if asyncio.iscoroutine(coro):
-				if loop:
-					loop.create_task(coro)
-				else:
-					coro.close()
+		listeners = self.listeners
+		i = len(listeners)
+		for listener in listeners[::-1]:
+			i -= 1
+			if listener(*args, **kwargs):
+				del listeners[i]
